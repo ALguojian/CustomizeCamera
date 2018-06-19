4e5ecac543f682a879629c55c23c7885f1884dcf
ALguojian <3324883037@qq.com>
2018-06-19 14:46:47 +0800

    添加加载预览是的loading

--- customizecamera/src/main/java/com/alguojian/customizecamera/activity/LookPictureActivity.java
+++ customizecamera/src/main/java/com/alguojian/customizecamera/activity/LookPictureActivity.java
@@ -2,15 +2,29 @@ package com.alguojian.customizecamera.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alguojian.customizecamera.InterFace.PreViewImageViewListener;
 import com.alguojian.customizecamera.R;
+import com.alguojian.imagegesture.ImageGesture;
 import com.alguojian.imagegesture.view.PhotoView;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
+import com.guojian.aldialog.dialog.LoadingDialog;
 
 public class LookPictureActivity extends AppCompatActivity {
 
@@ -41,8 +55,37 @@ public class LookPictureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_look_picture);
         initView();
 
+        final LoadingDialog loadingDialog = new LoadingDialog(LookPictureActivity.this, 2);
+
+        loadingDialog.show();
+
+        RequestOptions requestOptions = new RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+        RequestListener<Drawable> bitmapRequestListener = new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+
+                loadingDialog.dismiss();
+                Toast.makeText(LookPictureActivity.this,"加载失败",Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+
+                loadingDialog.dismiss();
+
+                return false;
+            }
+        };
+
         Glide.with(this)
                 .load(mStrin)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .listener(bitmapRequestListener)
+                .apply(requestOptions)
                 .into(previewImage);
 
         finish.setOnClickListener(v -> finish());
